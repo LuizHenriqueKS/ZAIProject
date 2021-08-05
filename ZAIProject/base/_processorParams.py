46ff69343e2d3bff4a62d752293aba553e0d64aa
Luiz Henrique Kafer Silva <luizhenrique-rs@windowslive.com>
2021-08-04 22:26:46 -0300

    Update _processorParams.py

--- ZAIProject/base/_processorParams.py
+++ ZAIProject/base/_processorParams.py
@@ -1,7 +1,10 @@
+from typing import Literal
+
+
 class ProcessorParams:
 
-    def __init__(self, mode: str, io: str, contextIteration=0, context=None):
-        self.mode = mode
-        self.io = io
-        self.contextIteration = contextIteration
-        self.context = context
+  def __init__(self, mode: Literal['scale', 'fit', 'apply'], io: Literal['input', 'target', 'output'], contextIteration=0, context=None):
+    self.mode = mode
+    self.io = io
+    self.contextIteration = contextIteration
+    self.context = context
